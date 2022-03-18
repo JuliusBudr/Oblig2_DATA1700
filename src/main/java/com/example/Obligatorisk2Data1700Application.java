07d79e0e7485f39447e215c8c1960f663ca27a34
JuliusBudr <bud.julius@gmail.com>
2022-03-18 09:52:50 +0100

    Oppretta Spring prosjekt

--- src/main/java/com/example/Obligatorisk2Data1700Application.java
+++ src/main/java/com/example/Obligatorisk2Data1700Application.java
@@ -1,11 +1,10 @@
-package com.example.obligatorisk2_data1700;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.example.obligatorisk2_data1700"})
+@SpringBootApplication
 public class Obligatorisk2Data1700Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
