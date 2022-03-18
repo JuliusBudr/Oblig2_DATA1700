e151910600a5472ed4ac6b583c2c3bed00ae332d
JuliusBudr <bud.julius@gmail.com>
2022-03-18 09:20:33 +0100

    Oppretta Spring prosjekt

--- src/main/java/com/example/obligatorisk2_data1700/Obligatorisk2Data1700Application.java
+++ src/main/java/com/example/obligatorisk2_data1700/Obligatorisk2Data1700Application.java
@@ -2,9 +2,10 @@ package com.example.obligatorisk2_data1700;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Obligatorisk2Data1700Application {
+public class Obligatorisk2Data1700Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Obligatorisk2Data1700Application.class, args);
